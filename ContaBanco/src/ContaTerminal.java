11db802ca40ee24e91b24ba7639aa7d58e5488b8
Adler <szadleersz@gmail.com>
2024-06-19 15:24:07 -0300

    Inclusao de requisicao de dados

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Conhecer e importar a classe scanner
+        //Exibir as mensagens para o usuário
+        //Obter pelo scanner os valores digitados no terminal
+        //Exibir a mensagem da conta criada
+
+        Scanner scanner = new Scanner();
+
+        int numeroConta;
+        int agencia;
+        String nomeCliente;
+        float saldo;
+
+        System.out.println("Por favor, digite o número da sua conta");
+        numeroConta = scanner.nextInt();
+        System.out.println("Por favor, digite o número da sua agência");
+        agencia = scanner.nextInt();
+        System.out.println("Por favor, digite o seu nome completo");
+        nomeCliente = scanner.next(nomeCliente);
+        System.out.println("Por favor, informe o valor que você depositará para a criação da sua conta:");
+        saldo = scanner.nextFloat();
+
+        System.out.println("Olá," + nomeCliente + "! Obrigado por criar uma conta em nosso banco, o número da sua agência é " + agencia + " o número da sua conta é " + numeroConta + " e seu saldo atual de: " + saldo + " já está disponível para saque!");
     }
 }
