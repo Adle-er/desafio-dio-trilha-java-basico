8ce698c742fbdc67c33cda07a95a9ea0dcb58b41
Adler <szadleersz@gmail.com>
2024-06-19 15:37:34 -0300

    Alteracao de tipo de dado do saldo

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,28 +1,32 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         //Conhecer e importar a classe scanner
         //Exibir as mensagens para o usuário
         //Obter pelo scanner os valores digitados no terminal
         //Exibir a mensagem da conta criada
 
-        Scanner scanner = new Scanner();
+         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         int numeroConta;
         int agencia;
         String nomeCliente;
-        float saldo;
+        float saldo = 0f;
 
         System.out.println("Por favor, digite o número da sua conta");
         numeroConta = scanner.nextInt();
         System.out.println("Por favor, digite o número da sua agência");
         agencia = scanner.nextInt();
         System.out.println("Por favor, digite o seu nome completo");
-        nomeCliente = scanner.next(nomeCliente);
+        nomeCliente = scanner.next();
         System.out.println("Por favor, informe o valor que você depositará para a criação da sua conta:");
-        saldo = scanner.nextFloat();
+        saldo = scanner.nextFloat();        
 
         System.out.println("Olá," + nomeCliente + "! Obrigado por criar uma conta em nosso banco, o número da sua agência é " + agencia + " o número da sua conta é " + numeroConta + " e seu saldo atual de: " + saldo + " já está disponível para saque!");
+
+        scanner.close();
     }
+    
 }
